4f37f623c148cd36014ef776e179bd3258bb10bd
KeLi <keliwork@outlook.com>
2020-12-02 16:26:24 +0800

    Updates code.

---
+++ KeLi.ReplaceWithSingleCallToCountBug.App/LinqExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace KeLi.ReplaceWithSingleCallToCountBug.App
+{
+    public static class LinqExtension
+    {
+        public static bool In<T>(this T value, params T[] ranges)
+        {
+            return ranges.Contains(value);
+        }
+    }
+}
---
+++ KeLi.ReplaceWithSingleCallToCountBug.App/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace KeLi.ReplaceWithSingleCallToCountBug.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ShowRightCase();
+            ShowWrongCase();
+        }
+
+        private static void ShowRightCase()
+        {
+            //var fruits = new List<string>()
+            //{
+            //    "Apple",
+            //    "Banana",
+            //    "Peach",
+            //    "Orange"
+            //};
+
+            // Raw code.
+            //return fruits.Where(w => w.In("Apple", "Peach")).Count() == 2;
+
+            // Sugget step 1.
+            //return fruits.Count(w => LinqToObjectExtension.In(w, "Apple", "Peach")) == 2;
+
+            // Sugget step 2.
+            //return fruits.Count(w => w.In("Apple", "Peach")) == 2;
+        }
+
+        private static void ShowWrongCase()
+        {
+            //var persons = new[]
+            //{
+            //    new { Fruit = "Apple", Name = "Jake" },
+            //    new { Fruit = "Peach", Name = "Tony" },
+            //};
+
+            //Raw code.
+            //return persons.Where(w => w.Name.In("Apple", "Peach")).Count() == 2;
+
+            //Sugget step 1.
+            //return persons.Count(w => LinqToObjectExtension.In("Apple", "Peach")) == 2;
+
+            //Sugget step 2.
+            //return persons.Count(w => "Apple".In("Peach")) == 2;
+        }
+    }
+}
